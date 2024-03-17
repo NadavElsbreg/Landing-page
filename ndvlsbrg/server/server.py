1afd293f142d6492bec6c06022f433bbe75222bc
NadavETheFirst <nadav.elsberg1@gmail.com>
2024-03-17 18:49:01 +0200

    added some comments and reorginize

--- ndvlsbrg/server/server.py
+++ ndvlsbrg/server/server.py
@@ -1,6 +1,7 @@
 from flask import Flask, request, jsonify
 from flask_cors import CORS
 from pymongo import MongoClient
+import datetime
 
 app = Flask(__name__)
 CORS(app)  # Enable CORS for all routes
@@ -12,24 +13,26 @@ LogCollection = db["LogInttempsLog"]
 loadsLogCollection = db["logLoads"]
 
 
-def logLogin(username, ipAddress, status):
-    LogCollection.insert_one({"username":username, "ipAddress":ipAddress, "status":status})
+def logLogin(username, ipAddress, status,time):
+    LogCollection.insert_one({"username":username, "ipAddress":ipAddress, "status":status, "time":time})
 
 def logLoads(ipAddres, agent, browser):
     loadsLogCollection.insert_one({"loadFrom":ipAddres, "agent":agent, "browser":browser})
 
 def perform_login(username, password, ipAddress):
+    current_time = datetime.datetime.now()
+    print("Current time of login:", current_time)
     user = CredentialCollection.find_one({"UserName": username})
     if user:
         if user["password"] == password:
-            logLogin(username,ipAddress,"succes")
-            return {"username": username, "Name": user["Name"]}
+            logLogin(username,ipAddress,"succes",current_time)
+            return {"username": username, "Name": user["Name"], "time":current_time}
         else:
-            logLogin(username,ipAddress,"failed")
-            return {"err": "Incorrect password"}
+            logLogin(username,ipAddress,"failed",current_time)
+            return {"err": "Incorrect password", "time":current_time}
     else:
-        logLogin(username,ipAddress,"No such user")
-        return {"err": "User not found"}
+        logLogin(username,ipAddress,"No such user",current_time)
+        return {"err": "User not found", "time":current_time}
 
 
 @app.route('/login', methods=['POST'])
@@ -46,7 +49,9 @@ def load():
     data =request.json
     ipAddress = data.get("ipAddress")
     agent = data.get("agent")
-    browser = data.get("browser")
+    browser = {"browserType":data.get("browserType"),
+               "version":data.get("version"),
+               "Ismobile":data.get("Ismobile"),}
     print("page loaded in ip address: ", ipAddress, " Browser: ",browser)
     print("aget: ", agent)
     logLoads(ipAddress, agent, browser)
--- ndvlsbrg/src/App.js
+++ ndvlsbrg/src/App.js
@@ -1,6 +1,4 @@
-import React ,{useState} from 'react';
-import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
-import logo from './logo.svg';
+import React, { useState } from 'react';
 import './css/App.css';
 import "./headers"
 import headerMainPage from './headers';
@@ -8,74 +6,77 @@ import mainBody from './body';
 import GenerateFooter from './footers'
 import Login from './LoginPage';
 import IPAddressFetcher from './IpAddressFetcher';
+//import { json } from 'body-parser';
 
+
+//gets the browser verion and agent to log in db
 var BrowserAgent = navigator.userAgent;
 var BrowserVersion = GetBrowserVersion();
-
 
 function GetBrowserVersion() {
   var userAgent = navigator.userAgent;
   var isMobile = /Mobile/.test(userAgent);
-  
+
   var browserType;
   var version;
-  
+
   if (/OPR/.test(userAgent)) {
     browserType = "Opera";
     version = userAgent.match(/OPR\/(\S+)/)[1];
-} else if (/Chrome/.test(userAgent)) {
+  } else if (/Chrome/.test(userAgent)) {
     browserType = "Google Chrome";
     version = userAgent.match(/Chrome\/(\S+)/)[1];
-} else if (/Safari/.test(userAgent)) {
+  } else if (/Safari/.test(userAgent)) {
     browserType = "Safari";
     version = userAgent.match(/Version\/(\S+)/)[1];
-} else if (/Firefox/.test(userAgent)) {
+  } else if (/Firefox/.test(userAgent)) {
     browserType = "Mozilla Firefox";
     version = userAgent.match(/Firefox\/(\S+)/)[1];
-} else if (/Edge/.test(userAgent)) {
+  } else if (/Edge/.test(userAgent)) {
     browserType = "Microsoft Edge";
     version = userAgent.match(/Edge\/(\S+)/)[1];
-} else if (/Trident/.test(userAgent)) {
+  } else if (/Trident/.test(userAgent)) {
     browserType = "Internet Explorer";
     version = userAgent.match(/rv:(\S+)/)[1];
-} else {
+  } else {
     browserType = "Unknown";
     version = "Unknown";
+  }
+
+  return  { browserType: browserType, Version: version, Ismobile: isMobile};
 }
 
-  return browserType + ", Version " + version + ", Ismobile=" + isMobile;
-
-}
-
-
-
+//logs browser version in db
 const PageLoad = async (ipAddress) => {
+  console.log("type of browser car: ", typeof(BrowserVersion));
   try {
-    
+
     await fetch('http://84.229.249.59:5000/load', {
       method: 'POST',
       headers: {
         'Content-Type': 'application/json',
-      },
-      body: JSON.stringify({ipAddress: ipAddress, agent:BrowserAgent, browser:BrowserVersion}),
+      },      
+      body: JSON.stringify({ ipAddress: ipAddress, agent: BrowserAgent, browserType: BrowserVersion["browserType"], Version: BrowserVersion["version"], Ismobile: BrowserVersion["isMobile"]}),
     });
   } catch (error) {
     console.error('Error:', error);
   }
-};
+}
 
-function App() { 
-   const [ipAddress, setIPAddress] = useState(null);
-   PageLoad(ipAddress);
-    return(
-      <div>
-        {IPAddressFetcher(setIPAddress)}
-        {headerMainPage()}
-        {Login(ipAddress)}
-        {mainBody(ipAddress)}
-        {GenerateFooter()}
-      </div>
-    );
+
+//actaul app fauntion that builds the entire page 
+function App() {
+  const [ipAddress, setIPAddress] = useState(null);
+  PageLoad(ipAddress);
+  return (
+    <div>
+      {IPAddressFetcher(setIPAddress)}
+      {headerMainPage()}
+      {Login(ipAddress)}
+      {mainBody(ipAddress)}
+      {GenerateFooter()}
+    </div>
+  );
 }
 
 export default App;
